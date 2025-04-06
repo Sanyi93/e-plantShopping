fffc79e9aa73d1986089f74fefd9cee58e070d88
Sanyi93 <lalalala@tutututututututt.md>
2025-04-06 08:20:11 -0400

    addToCart button modified

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -323,7 +323,13 @@ function ProductList({ onHomeClick }) {
                                 {/*Similarly like the above plant.name show other details like description and cost*/}
                                 <div className="product-desciption">{plant.description}</div>
                                 <div className="product-price">{plant.cost}</div>
-                                <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' :''}`}
+                                        style={{backgroundColor: addedToCart[plant.name] ? 'gray': 'green'}} 
+                                        onClick={() => handleAddToCart(plant)}
+                                        disabled={addedToCart[plant.name]}
+                                        >
+                                        {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
+                                </button>
                             </div>
                             ))}
                         </div>
